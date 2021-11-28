c595f7f9550ecc7d470f423acc9c0533e3f0637a
Orlando Cruz <1502834@estudante.uab.pt>
2021-11-28 13:53:43 +0000

    Remover e tecla X

--- src/three.js
+++ src/three.js
@@ -4,10 +4,13 @@ import { GUI } from 'https://unpkg.com/three@0.124.0/examples/jsm/libs/dat.gui.m
 import {lineMP}	from '../../lineMP.mjs'
 
 var renderer, scene, controls;
-
 const raycaster = new THREE.Raycaster();
 const posicaoRato_2d = new THREE.Vector2();
 
+var Pontos_Amarelos=[];
+var pixel_inicial,pixel_final;
+var bufferMaterial_pixel_inicial,bufferMaterial_pixel_final;
+var contador=0;
 
 //Camera
 var camera;
@@ -42,7 +45,7 @@ camera.position.set(0, 0, 14);
 controls = new OrbitControls(camera, renderer.domElement);
 controls.enablePan = false;
 
-Recentrar();
+Opcoes();
 }
 	  
 function animate() {
@@ -71,7 +74,7 @@ function CriarPadrao(){
 //Criar um display raster
 function DisplayRaster(){
 
-	//20 quadrados em ambas as direções
+	//21x21
 	var x=-10;			// x inicial.
 	var y=-10;			// y inicial.
 	var x_f=11;			// x final.
@@ -153,25 +156,24 @@ function onMouseMove( event ) {
 
 	posicaoRato_2d.x =   (event.clientX / renderer.domElement.clientWidth) * 2 - 1,
 	posicaoRato_2d.y = -(event.clientY / renderer.domElement.clientHeight) * 2 + 1,
-	//console.log("x:",posicaoRato_2d.x,"  y:",posicaoRato_2d.y);
 	render();
 }
 
 function render() {
 
-	// update the picking ray with the camera and mouse position
+	// posição tendo em conta a camera e posição do rato
 	raycaster.setFromCamera( posicaoRato_2d, camera );
 
-	//3. compute intersections
-	var intersects = raycaster.intersectObjects(scene.children);
+	//Obter os objectos intersetado
+	var intersectados = raycaster.intersectObjects(scene.children);
 
-	for (var i = 0; i < intersects.length; i++) {
-	   console.log(intersects[i].name);
-	   let obj = intersects[i];
-	   //scene.remove(scene.children[i]);                                                                        //removendo os objetos desnecessários
+
+	for (var i = 0; i < intersectados.length; i++) {
+	   let obj = intersectados[i];               
         if(obj.object.name === "Pixel"){
 			console.log("x:",obj.object.position.x,"  y:",obj.object.position.y)
-                //scene.remove(scene.children[i]); 
+			if(contador<2)
+				contador==0?pixel_inicial=obj: pixel_final=obj;
 		}   
 	}
 	renderer.render( scene, camera );
@@ -184,7 +186,104 @@ function recentrar() {
 	controls.reset();
 }
 
-function Recentrar(){
+function limpar_pixels(){
+	contador=0;
+	if(pixel_inicial)
+	pixel_inicial.object.material=bufferMaterial_pixel_inicial;
+	pixel_final.object.material=bufferMaterial_pixel_final;
+	
+	var Pixel_rem,Linha_rem;
+
+	do{
+		Pixel_rem=scene.getObjectByName("Pixel_Amarelo");
+		Linha_rem=scene.getObjectByName("Linha");
+		scene.remove(Pixel_rem);
+		scene.remove(Linha_rem);
+
+	}while(Pixel_rem || Linha_rem)
+	//scene.remove(scene.getObjectByName("Pixel_Amarelo"));
+	//scene.remove(scene.getObjectByName("Linha");
+}
+
+window.addEventListener("keydown", Tecla_Pressionada, false);
+
+function Tecla_Pressionada (event) {
+    var TeclaCod = event.which;
+	//tecla x
+    if (TeclaCod == 88 && contador<2) {
+		if(contador==0){
+			bufferMaterial_pixel_inicial=pixel_inicial.object.material;	//Guardar material original
+			var NovoMaterial=bufferMaterial_pixel_inicial.clone();		//criar um novo material
+			NovoMaterial.color.setHex( 0xff0000 );						//mudar a cor do pixel
+			pixel_inicial.object.material=NovoMaterial;					//Associar o novo material ao objecto
+			contador++;
+		}
+		else if(contador==1){
+			bufferMaterial_pixel_final=pixel_final.object.material;		//Guardar material original
+			var NovoMaterial=bufferMaterial_pixel_final.clone();		//criar um novo material
+			NovoMaterial.color.setHex( 0xff0000 );						//mudar a cor do pixel
+			pixel_final.object.material=NovoMaterial;					//Associar o novo material ao objecto
+			
+			Criar_Reta(pixel_inicial.object.position.x,				//Criar Linha
+						pixel_inicial.object.position.y,
+						pixel_final.object.position.x,
+						pixel_final.object.position.y,
+						)															
+			contador=0;
+		}
+	} 
+	//tecla backspace
+	else if (TeclaCod == 8) {
+		limpar_pixels();
+	}
+        
+};
+
+function Criar_Reta(pi_x,pi_y,pf_x,pf_y){
+	var P1={x:pi_x , y: pi_y};
+	var P2={x:pf_x , y: pf_y};
+
+	var pontos=lineMP(P1,P2);
+
+	const material = new THREE.MeshBasicMaterial( 
+		{color: 0xDDEE00, 
+		 side: THREE.DoubleSide,
+		 opacity:0.4,
+		 transparent:true
+		});
+
+	CriarLinha(pi_x,pf_x,pi_y,pf_y);	
+
+	for(var p in pontos){
+		//Pixel com dimensão x=1, y=1
+		const geometria_pixel = new THREE.PlaneGeometry( 1, 1 );
+		//Cria o pixel atribuindo o material e posição. 
+		const pixel = new THREE.Mesh( geometria_pixel, material);
+		pixel.position.set(pontos[p].x,pontos[p].y,1/4);
+		pixel.name="Pixel_Amarelo";
+		scene.add(pixel);
+	}	
+}
+
+function CriarLinha(x_inicial, x_final, y_inicial, y_final){
+
+	//Caracteristicas da linha a ser representada
+	const material_linha = new THREE.LineBasicMaterial({color: 0x990000});						//cor e caracteristicas visuais
+	var pontos = [];
+	
+	pontos.push( new THREE.Vector3( x_inicial, y_inicial, 0 ) );								//array com as cordenadas de inicio e fim
+	pontos.push( new THREE.Vector3( x_final, y_final, 0 ) );									//da linha a ser apresentada
+
+	var geometria_linha = new THREE.BufferGeometry().setFromPoints( pontos )					//geometria da linha definida por pontos
+
+	var linha = new THREE.Line( geometria_linha, material_linha );
+	linha.name="Linha";
+
+	scene.add(linha);
+}
+
+
+function Opcoes(){
 	
 	var gui = new GUI ({ autoPlace: false });
 	gui.domElement.id = 'gui';
@@ -192,21 +291,27 @@ function Recentrar(){
 
 	//Criar "botão" para centralizar o plano
 	var parametros = {
-		Centrar: recentrar
+		Centrar: recentrar,
+		Limpar: limpar_pixels
 	};
 	
-	//var folder = gui.addFolder('My folder');
+	var folder = gui.addFolder('Opções');
 	
 	gui.add(parametros, 'Centrar');
+	gui.add(parametros, 'Limpar')
 	gui.open();
 }
 
-//Redimensionar a janela
-window.addEventListener('resize', onWindowResize, false)
 
+
+//Redimensionar janela
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
     renderer.setSize(window.innerWidth, window.innerHeight)
     render()
 }
+
+//Redimensionar EventListener
+window.addEventListener('resize', onWindowResize, false)
+
